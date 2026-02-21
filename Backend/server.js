242e8b4e5f5314d6205e02f295375ed69d0522df
mannnJAVA <mannn.html@gmail.com>
2026-02-21 09:14:33 +0530

    server.js edited not loading database

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,10 @@
-require("dotenv").config();
+// require("dotenv").config();
 const app = require("./src/app");
 
 const PORT = process.env.PORT || 5001;
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
