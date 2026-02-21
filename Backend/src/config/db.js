618bbd4f7510cbbf0d896ffa762b6383615c8e87
mannnJAVA <mannn.html@gmail.com>
2026-02-21 09:18:23 +0530

    db.js updated

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,27 +1,3 @@
-const { Pool } = require("pg");
-
-const isProduction = process.env.DATABASE_URL;
-
-const pool = new Pool(
-  isProduction
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-    : {
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-      },
-);
-
-pool
-  .connect()
-  .then(() => console.log("Database connected successfully"))
-  .catch((err) => console.error("Database connection error:", err));
-
-module.exports = pool;
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
